5c3285f2aeab0a592d47b2c8d985f4cc747dcfd8
rounakc <rounak.chakraborty02@gmail.com>
2024-06-08 00:36:47 +0530

    url changed

--- client/src/apicalls/axiosInstance.js
+++ client/src/apicalls/axiosInstance.js
@@ -1,7 +1,7 @@
 
 import axios from 'axios';
 export const axiosInstance = axios.create({
-    baseURL:  'http://localhost:5000',
+    baseURL:  'https://julxs.onrender.com',
     headers : {
          authorization : `Bearer ${localStorage.getItem('token')}`
     }
